b49e48dc96be423953f2616a9cc30f24c4e6940b
Enrique Ray Rodriguez <enriquerayrodriguez@gmail.com>
2025-07-07 23:11:42 +0800

    Update booking.php

--- admin/booking.php
+++ admin/booking.php
@@ -1,7 +1,7 @@
 <?php
     if($_SERVER["REQUEST_METHOD"] == "POST"){
-        $check_in = $_POST['check_in'] ?? '';
-        $check_out = $_POST['check_out'] ?? '';
+        $checkin = $_POST['check_in'] ?? '';
+        $checkout = $_POST['check_out'] ?? '';
         $room = $_POST['room'] ?? '';
 
         echo "<h2>Thank you for booking at Skyview Resort!</h2>";
