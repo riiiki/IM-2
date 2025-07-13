45de72685623a2fb10f9696547e24e7c54b87c3b
Enrique Ray Rodriguez <enriquerayrodriguez@gmail.com>
2025-07-14 00:06:37 +0800

    Add files via upload

--- admin/user.php
+++ admin/user.php
@@ -7,26 +7,27 @@ include "header.php";
 
 $result = $conn->query("SELECT * FROM Customer ORDER BY DateRegistered DESC");
 ?>
-
-<section class="welcome">
-  <h2>Registered Users</h2>
-  <div class="table_responsive">
-    <table>
-        <th>Full Name</th>
-        <th>Email</th>
-        <th>Phone</th>
-        <th>Date Registered</th>
-      </tr>
-      <?php while ($row = $result->fetch_assoc()): ?>
-        <tr>
-          <td><?= htmlspecialchars($row['FullName']) ?></td>
-          <td><?= $row['Email'] ?></td>
-          <td><?= $row['PhoneNumber'] ?></td>
-          <!-- <td><?= $row['DateRegistered'] ?></td> tentative pani siya -->
+<div class="wrapper">
+  <section class="welcome">
+    <h2>Registered Users</h2>
+    <div class="table_responsive">
+      <table>
+          <th>Full Name</th>
+          <th>Email</th>
+          <th>Phone</th>
+          <th>Delete User</th>
         </tr>
-      <?php endwhile; ?>
-    </table>
-  </div>
-</section>
+        <?php while ($row = $result->fetch_assoc()): ?>
+          <tr>
+            <td><?= htmlspecialchars($row['FullName']) ?></td>
+            <td><?= $row['Email'] ?></td>
+            <td><?= $row['PhoneNumber'] ?></td>
+            <!-- <td><?= $row['DateRegistered'] ?></td> tentative pani siya -->
+          </tr>
+        <?php endwhile; ?>
+      </table>
+    </div>
+  </section>
+</div>
 
 <?php include "footer.php"; ?>
