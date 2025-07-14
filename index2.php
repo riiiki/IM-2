664e9216892c2fdfda2e0ac9085873b0a7363f80
Pax <143660878+vinleopacs@users.noreply.github.com>
2025-07-15 00:44:34 +0800

    Update index2.php

--- index2.php
+++ index2.php
@@ -6,6 +6,8 @@ include("user/auth.php");
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+  <link rel="icon" type="image/png" href="images/mountain.png">
   <title>Skyview Resort</title>
   <link rel="stylesheet" href="css/style.css" />
 </head>
@@ -64,6 +66,16 @@ include("user/auth.php");
     <footer>
       <p>© 2025 Skyview Resort. All rights reserved.</p>
       <p>📞 +63 960 863 2989</p>
+      <div class="social-links">
+        <h4>Stay Connected</h4>
+        <div class="icon-buttons">
+          <a href="https://www.facebook.com/profile.php?id=100063647214137" target="_blank" class="icon facebook"><i class="fab fa-facebook-f"></i></a>
+          <a href="https://twitter.com" target="_blank" class="icon twitter"><i class="fab fa-twitter"></i></a>
+          <a href="https://ph.pinterest.com/pin/30328997485654246/" target="_blank" class="icon pinterest"><i class="fab fa-pinterest-p"></i></a>
+          <a href="https://www.instagram.com/islandskyview.resort/?hl=en" target="_blank" class="icon instagram"><i class="fab fa-instagram"></i></a>
+          <a href="http://youtube.com/@MrBeast" target="_blank" class="icon youtube"><i class="fab fa-youtube"></i></a>
+        </div>
+      </div>
     </footer>
   </div>
   <script src="../../node.js"></script>
