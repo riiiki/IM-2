14538e9d45a5a52e3faec775b75a330601e956f6
Enrique Ray Rodriguez <enriquerayrodriguez@gmail.com>
2025-07-15 22:35:38 +0800

    Revert "Add files via upload"

--- admin/footer.php
+++
@@ -1,8 +0,0 @@
-  </main>
-  <footer style="text-align: center; background-color: #003366; color: white; padding: 1rem;">
-    <p>© 2025 Skyview Resort Admin Panel</p>
-    <p>📞 +63 960 863 2989</p>
-  </footer>
-</div>
-</body>
-</html>
